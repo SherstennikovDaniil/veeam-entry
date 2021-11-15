f7d83b69400723fafc47084bc4909bbcd49ff042
SherstennikovDaniil <sherswrk@yandex.ru>
2021-11-15 23:17:31 +0300

    Forgot about file descriptors

--- task01/main.py
+++ task01/main.py
@@ -1,12 +1,13 @@
 import psutil
 import os
+import datetime
 from time import sleep
 import csv
 
 
 def track(path: str, interval: int) -> None:
     p = os.path.normpath(path)  # removed abspath so can run any terminal commands
-    name = ""
+    name = p.split("/")[0]
 
     # shell=True and chmod +x fixed permission issues
     pid = psutil.Popen(p, shell=True).pid
@@ -17,6 +18,7 @@ def track(path: str, interval: int) -> None:
             mem = proc.memory_full_info()
             rss = mem[0]
             vms = mem[1]
+            fds = proc.num_fds()
         sleep(interval)
     pass
 

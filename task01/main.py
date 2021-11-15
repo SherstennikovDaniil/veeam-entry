651fbfcee49e75c333c9cd1ae4b8f2fa844fdfb3
SherstennikovDaniil <sherswrk@yandex.ru>
2021-11-15 23:49:02 +0300

    Made init for Writer class

--- task01/main.py
+++ task01/main.py
@@ -7,25 +7,36 @@ import csv
 
 def track(path: str, interval: int) -> None:
     p = os.path.normpath(path)  # removed abspath so can run any terminal commands
-    name = p.split("/")[0]
-
+    name = (
+        p.split("/")[0] + datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S") + ".csv"
+    )
+    print(name)
     # shell=True and chmod +x fixed permission issues
     pid = psutil.Popen(p, shell=True).pid
     proc = psutil.Process(pid)
     while True:
-        with proc.oneshot():
-            cpu = proc.cpu_percent()
-            mem = proc.memory_full_info()
-            rss = mem[0]
-            vms = mem[1]
-            fds = proc.num_fds()
-        sleep(interval)
+        try:
+            with proc.oneshot():
+                cpu = proc.cpu_percent()
+                mem = proc.memory_full_info()
+                rss = mem[0]
+                vms = mem[1]
+                fds = proc.num_fds()
+            sleep(interval)
+        except psutil.AccessDenied:
+            print("Process finished.")
+            break
     pass
 
 
 class Writer:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name) -> None:
+        self.name = name
+        with open(self.name, "w", newline="") as f:
+            writer = csv.writer(
+                f, delimiter=":", quotechar="|", quoting=csv.QUOTE_MINIMAL
+            )
+            writer.writerow(["CPU", "RSS", "VMS", "FDS"])
 
     def write(cpu, rss, vms) -> None:
         pass

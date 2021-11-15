fb99ca5e674b5da3f16b709066a874c348d92459
SherstennikovDaniil <sherswrk@yandex.ru>
2021-11-15 21:46:51 +0300

    Slowly figuring the way to do it better

--- task01/main.py
+++ task01/main.py
@@ -1,1 +1,14 @@
+from typing import List, Mapping
 import psutil
+import os
+from time import sleep
+
+
+def track(path: str, interval: int) -> List:
+    return
+
+
+if __name__ == "__main__":
+    path = input("Введите путь: ")
+    _int = int(input("Введите интервал: "))
+    track(p=path, interval=_int)
